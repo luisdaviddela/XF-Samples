d3f2936b2d367912b4a25da1b17551368747c509
Luis David De la cruz <delacruz_luis@outlook.es>
2018-06-18 22:57:14 -0500

    3j

---
+++ LDXF/LDXF/LDXF/Ej3.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LDXF
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Ej3 : ContentPage
+	{
+		public Ej3 ()
+		{
+			InitializeComponent ();
+            yu.Items.Add("xsluis");
+		}
+	}
+}
